1a0bc72490913dfc3ce9f4f60a5472aae839adf8
Epowerj <epowerj@gmail.com>
2018-06-25 13:30:25 -0400

    added simulation function

--- src/main/java/SimulationGameState.java
+++ src/main/java/SimulationGameState.java
@@ -8,4 +8,24 @@ public class SimulationGameState extends GameState {
     public void copy(GameState other) { // makes copy public on this class
         super.copy(other);
     }
+
+    public int simulateUntilLoss() {
+        int turns = 0;
+        int player = 0;
+
+        while (!haveLost()) {
+            turns++;
+
+            newTurn(players.get(player));
+
+            //switch to the next player
+            if (player < players.size()) {
+                player++;
+            } else {
+                player = 0;
+            }
+        }
+
+        return turns;
+    }
 }
